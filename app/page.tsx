243b352af31fa60db3c20234319eee51b3b000c9
kolev <marjan.kolev@gmail.com>
2025-11-16 15:18:37 +0100

    Add Toast component to Home page for user notifications

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+'use client';
+
 import { GlobalContextProvider } from '@/context';
 import { Paint } from '@/components/Paint';
+import { Toast } from '@/components/Toast';
 
 export default function Home() {
   return (
     <GlobalContextProvider>
+      <Toast />
       <div className="content">
         <Paint />
       </div>
